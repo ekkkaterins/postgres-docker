85c807408f1d29818c62b3ae2318c83e36942f14
ekkkaterins <zelenova628@gmail.com>
2025-04-19 08:00:00 +0000

    Table

--- init.sql
+++ init.sql
@@ -5,25 +5,25 @@ CREATE TABLE IF NOT EXISTS Students (
     name TEXT NOT NULL,
     start_year INT
 );
-CREATE TABLE IF NOT EXISTS Couses (
+CREATE TABLE IF NOT EXISTS Courses (
     c_no SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     hours INT
 );
-CREATE TABLE IF NOT EXISTS exam (
+CREATE TABLE IF NOT EXISTS Exams (
     id SERIAL PRIMARY KEY,
     s_id INT REFERENCES Students(s_id) ON DELETE CASCADE,
     c_no INT REFERENCES Courses(c_no) ON DELETE CASCADE,
     score INT NOT NULL
 );
 
-INSERT INTO Students (name, stary_year) VALUES
+INSERT INTO Students (name, start_year) VALUES
     ('Kate', 2020),
     ('Jack', 2022);
 INSERT INTO Courses (title, hours) VALUES
     ('Math', 160),
     ('English', 200);
-INSERT INTO exam (s_id, c_no, score) VALUES
+INSERT INTO Exams (s_id, c_no, score) VALUES
     (1,1,99),
     (1,2,55),
     (2,1,67),
